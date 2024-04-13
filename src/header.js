f698cc2b50595ee64082eb7252c3cb6126fa5263
Adabsmith <53339704+adabsmith@users.noreply.github.com>
2024-04-13 10:09:08 -0400

    final navbar styling

--- src/header.js
+++ src/header.js
@@ -10,7 +10,7 @@ const CustomNavbar = () => {
       <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link active" href="/">Home</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/cars">Cars</a>
@@ -20,7 +20,7 @@ const CustomNavbar = () => {
           </li>
         </ul> 
       </div>
-      <button className="btn bg-dark my-2 my-sm-0 ml-3 d-flex justify-content-end" style={{ marginRight:'10px'}}>Book Now</button>
+      <button className="btn btn-dark my-2 my-sm-0 ml-3 d-flex justify-content-end" style={{ marginRight:'10px'}}>Book Now</button>
     </nav>
   );
 }
