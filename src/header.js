b24f553ef4f366b31f79e961dcdc6dbf575755e1
Adabsmith <53339704+adabsmith@users.noreply.github.com>
2024-04-13 10:01:10 -0400

    added CTA to navbar

--- src/header.js
+++ src/header.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const CustomNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">Car Rental</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary " >
+      <a className="navbar-brand" href="/" style={{ marginLeft:'10px'}}>Rent a Car</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a className="nav-link" href="/">Home</a>
@@ -16,10 +16,11 @@ const CustomNavbar = () => {
             <a className="nav-link" href="/cars">Cars</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">Contact</a>
+            <a className="nav-link" href="/contact">Team</a>
           </li>
-        </ul>
+        </ul> 
       </div>
+      <button className="btn bg-dark my-2 my-sm-0 ml-3 d-flex justify-content-end" style={{ marginRight:'10px'}}>Book Now</button>
     </nav>
   );
 }
