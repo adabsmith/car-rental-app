64eb3b4601fa668a0867a63df37456d0364637fe
Tactical-one <obinnachiedozie@yahoo.com>
2024-04-14 14:46:25 -0400

    FAQ content added (3)

--- src/faq.js
+++ src/faq.js
@@ -13,13 +13,13 @@ function Accordion() {
           />
           <AccordionItem
             id="flush-collapseTwo"
-            title="Accordion Item #2"
-            content="Placeholder content for this accordion, which is intended to demonstrate the  class. This is the second item's accordion body. Let's imagine this being filled with some actual content."
+            title="What type of Insurance do you provide?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie tempus lorem sed dictum. Duis imperdiet risus quis lacus sollicitudin, sed bibendum nibh ultrices."
           />
           <AccordionItem
             id="flush-collapseThree"
-            title="Accordion Item #3"
-            content="Placeholder content for this accordion, which is intended to demonstrate the  class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application."
+            title="How soon can I have the car collected after a booking?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie tempus lorem sed dictum. Duis imperdiet risus quis lacus sollicitudin, sed bibendum nibh ultrices."
           />
         </div>
       </div>
