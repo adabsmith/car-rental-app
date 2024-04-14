4ad0d49851322ef95af01b080471b7feb735ee49
Tactical-one <obinnachiedozie@yahoo.com>
2024-04-13 21:08:20 -0400

    car catalog photos updated (7 cars)

--- src/carList.js
+++ src/carList.js
@@ -3,14 +3,14 @@ import React from 'react';
 const CarList = () => {
   const cars = [
     { id: 1, name: 'Toyota Corolla', price: 50, image: 'toyota_corolla.avif' },
-    { id: 2, name: 'Honda Civic', price: 60, image: 'honda_civic.jpg' },
-    { id: 3, name: 'Ford Mustang', price: 80, image: 'ford_mustang.jpg' },
+    { id: 2, name: 'Honda Civic', price: 60, image: 'Honda-CivicSport.webp' },
+    { id: 3, name: 'Ford Mustang', price: 80, image: '2024_Ford_Mustang.jpg' },
     // Add more cars with their respective images
-    { id: 4, name: 'BMW X5', price: 100, image: 'bmw_x5.jpg' },
-    { id: 5, name: 'Mercedes-Benz E-Class', price: 120, image: 'mercedes_e_class.jpg' },
-    { id: 6, name: 'Audi Q7', price: 110, image: 'audi_q7.jpg' },
-    { id: 7, name: 'Chevrolet Camaro', price: 90, image: 'chevrolet_camaro.jpg' },
-    { id: 8, name: 'Volkswagen Golf', price: 70, image: 'vw_golf.jpg' },
+    { id: 4, name: 'BMW X5', price: 100, image: 'bmw-x5.jpg' },
+    { id: 5, name: 'Mercedes-Benz E-Class', price: 120, image: 'front-left-side-47.jpg' },
+    { id: 6, name: 'Audi Q7', price: 110, image: 'front-left-side-47.webp' },
+    { id: 7, name: 'Chevrolet Camaro', price: 90, image: 'singapore7.webp' },
+    { id: 8, name: 'Volkswagen Golf', price: 70, image: '20230602055539_2.jpg' },
     { id: 9, name: 'Tesla Model 3', price: 150, image: 'tesla_model_3.jpg' },
     { id: 10, name: 'Lexus RX', price: 130, image: 'lexus_rx.jpg' },
     { id: 11, name: 'Subaru Outback', price: 80, image: 'subaru_outback.jpg' },
