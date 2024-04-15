b4296147903001a0efa72670478c641b9a8d860d
Adabsmith <53339704+adabsmith@users.noreply.github.com>
2024-04-15 14:01:54 -0400

    fixed navigation bar responsive bugd

--- src/header.js
+++ src/header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import CarBooking from './carbooking';
 import { Link } from 'react-router-dom'
+import "./Nabvar.css"
 
 const CustomNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top " >
-      <a className="navbar-brand" href="/" style={{ marginLeft:'10px'}}> <b>Rent a Car</b></a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary sticky-top " >
+        <div className="container-fluid">
+      <a className="navbar-brand" href="#" style={{ marginLeft:'10px'}}> <b>Rent a Car</b></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+      <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" href="/">Home</a>
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/cars">Cars</a>
@@ -20,11 +22,13 @@ const CustomNavbar = () => {
           <li className="nav-item">
             <a className="nav-link" href="./Team">Team</a>
           </li>
-        </ul> 
-      </div>
-      <a href ='/carbooking' style={{textDecoration:"none"}}>
-      <button className="btn btn-dark my-2 my-sm-0 ml-3 d-flex justify-content-end" style={{ marginRight:'10px'}}>Book Now</button>
+        </ul>
+          <a href ='/carbooking' style={{textDecoration:"none"}}>
+      <button className="btn btn-dark my-2 my-sm-0 ml-3  justify-content-end" style={{ marginRight:'10px'}}>Book Now</button>
     </a>
+      </div>
+     
+    </div>
     </nav>
   );
 }
