9fa6cc3977c2320b171a1800814611126f9e2e26
Lolis007 <ladan.aisha@yahoo.com>
2024-04-14 23:34:26 -0400

    More members/cards added to Team section

--- src/Team.js
+++ src/Team.js
@@ -3,6 +3,9 @@ import ceo1 from "./ceo1.jpg";
 import Salee from "./Salee.jpg";
 import opr from "./opr.jpg";
 import staff from "./staff.jpg";
+import srep from "./srep.jpg";
+import lady from "./lady.jpg";
+import CEO from "./CEO.jpg"
 
 
 function Team() {
@@ -11,6 +14,12 @@ function Team() {
         <h2 className="h1 mb-5">Meet The Team</h2>
       <div className="container mt-24">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <Card
+            img={CEO}     
+            name="Micheal Hallender"
+            job="CEO"
+          />
           <Card
             name="Devon Klenjan, OALA, CSLA"
             job="Branch Manager"
@@ -18,19 +27,30 @@ function Team() {
           />
           <Card
             img={staff}     
-            name="Matte Crossman"
+            name="Kim Lee"
             job="Customer Service Representative"
           />
           <Card
             img={Salee}
-            name="Nathan Groat, CPA, CA"
+            name="Melissa Ralph, CPA, CA"
             job="Financial Manager"
           />
           <Card
             img={opr}
-            name="David Baartman"
-            job="Sales Representative"
+            name="Dan Brown"
+            job="Fleet Manager"
           />
+          <Card
+            img={srep}
+            name="Fatima Smith"
+            job="Marketing Coordinator"
+          />
+          {/* <Card
+            img={lady}
+            name="Jane Arron"
+            job="Sales Representative"
+          /> */}
+          
         </div>
       </div>
     </section>
