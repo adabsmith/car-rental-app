96b53454016a149f10ca8e5eb0f06ca41cc8c0ea
Tactical-one <obinnachiedozie@yahoo.com>
2024-04-15 17:54:33 -0400

    final car card added to grid

--- src/carList.js
+++ src/carList.js
@@ -18,6 +18,7 @@ const CarList = () => {
     { id: 13, name: 'Mercedes-Benz E400', price: 60, image: 'mercedes.png' },
     { id: 14, name: 'Ferrari', price: 60, image: 'Ferrari.jpg' },
     { id: 15, name: 'Lamborghini Aventador', price: 60, image: 'Lambo1.png' }, 
+    { id: 16, name: 'Porsche', price: 110, image: 'Lambo1.png' }, 
 
   ];
  
