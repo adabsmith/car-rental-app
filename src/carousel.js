13488e86dca27da018e33627666bc2d1a88b2ba5
Lolis007 <ladan.aisha@yahoo.com>
2024-04-14 22:44:44 -0400

    Modifications made to carosel images to fit veiw height

--- src/carousel.js
+++ src/carousel.js
@@ -5,21 +5,21 @@ import GrenadierImage from './Grenadier.jpg';
 
 function CarouselComponent() {
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel"style={{ paddingBottom: '1px'}}>
+    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" style={{ paddingBottom: '1px', height: '100dvh'}}>
       <div className="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-      <div className="carousel-inner" style={{height: '650px'}}>
-        <div className="carousel-item active">
-        <img src={GrenadierImage} className="d-block w-100" alt="Slide 1" style={{ height: '100%'}} />
+      <div className="carousel-inner w-100 h-100" >
+        <div className="carousel-item active" style={{  height: '100dvh'}}>
+        <img src={GrenadierImage} className="d-block w-100 object-fit-cover h-100" alt="Slide 1"  />
         </div>
-        <div className="carousel-item">
-          <img src={bentleyImage} className="d-block w-100" alt="Slide 2" />
+        <div className="carousel-item" style={{  height: '100dvh'}}>
+          <img src={bentleyImage} className="d-block w-100 object-fit-cover h-100" alt="Slide 2" />
         </div>
-        <div className="carousel-item">
-          <img src={mustangImage} className="d-block w-100" alt="Slide 3" />
+        <div className="carousel-item" style={{  height: '100dvh'}}>
+          <img src={mustangImage} className="d-block w-100 object-fit-cover " alt="Slide 3" />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
